a4865f4305eead948dc4de6312b0b02aeef8a48c
Gauravk2610 <gauravkonde26@gmail.com>
2022-12-31 11:26:21 +0530

    azyLoad added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,23 +2,13 @@ import axios from "axios";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
 import SpotLight from "../components/SpotLight";
-
-// - The section below spotlight will show the previous 7 items as per the design above
-// - Each item will show a title, author, and date
-// - Horizontal scroller with 7 items
-// - Clicking an item should open a new overlay that shows the selected item like
-// the spotlight section
-
-// - The next section will be same as the previous one, with 7 older items. You will be
-// using the `start_date` and `end_date` parameters in your API requests to fetch paged
-// results for these section
-// - The web-app will scroll infinitely, and make successive requests requests as the user
-// scrolls
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Home = () => {
   const [spotlight, setSpotlight] = useState({});
   const [previous, setPrevious] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [isFetching, setIsFetching] = useState(false);
 
   // set todays date in end date
@@ -50,8 +40,8 @@ const Home = () => {
     console.log(response.data, "enddatat");
 
     console.log([...response.data], "response", previous);
-      
-    console.log(previous.includes([...response.data]))
+
+    console.log(previous.includes([...response.data]));
 
     setPrevious((prev) => [...prev, [...response.data]]);
 
@@ -73,33 +63,35 @@ const Home = () => {
   // if it does, make a new request to fetch the next 7 items
   const handleScroll = useCallback(() => {
     if (
-      window.innerHeight + document.documentElement.scrollTop < document.documentElement.offsetHeight - 5 || 
-      window.innerHeight + document.documentElement.scrollTop > document.documentElement.offsetHeight + 5 ||
-      window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
+      window.innerHeight + document.documentElement.scrollTop <
+        document.documentElement.offsetHeight - 5 ||
+      window.innerHeight + document.documentElement.scrollTop >
+        document.documentElement.offsetHeight + 5 ||
+      window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
     ) {
-        setIsFetching(true);
+      setIsFetching(true);
     }
   }, []);
-
 
   useEffect(() => {
     if (!isFetching) return;
     getPreviousAPOD();
   }, [isFetching]);
-  
+
   const HorizontalScroll = ({ data }) => {
     const scrollRef = useRef(null);
-  
+
     const leftClick = () => {
       if (scrollRef.current.scrollLeft <= 0) return;
       scrollRef.current.scrollLeft -= 300;
     };
-  
+
     const rightClick = () => {
       if (scrollRef.current.scrollLeft >= scrollRef.current.scrollWidth) return;
       scrollRef.current.scrollLeft += 300;
     };
-  
+
     return (
       <div className="flex relative select-none">
         {/* left Icon for scroll */}
@@ -141,12 +133,12 @@ const Home = () => {
               ></iframe>
             ) : (
               <div className="w-[500px] h-[350px] relative">
-                  <img
+                <LazyLoadImage
                   key={index}
                   className="max-w-[500px] w-[70vw] sm:w-auto h-[350px] object-contain sm:object-none"
                   src={item?.url}
                   alt=""
-                  />
+                />
               </div>
             )
           )}
@@ -181,29 +173,23 @@ const Home = () => {
         <div className="slr space-y-8 px-12 min-h-screen mb-10">
           {previous.map((item, index) => (
             <HorizontalScroll key={index} data={item} />
-            ))}
-            {
-              loading && (
-              <div className="flex relative select-none">
-                <div className="slr relative gap-x-10 flex overflow-x-scroll scroll-smooth backdrop-blur-md shadow-lg py-2">
-                  {
-                    [...Array(7)].map((item, index) => (
-                      // skeleton loader
-                      <div key={index} className="w-[500px] h-[300px] relative">
-                        <div className="w-[350px] h-full bg-gray-400 animate-pulse"></div>
-                      </div>
-  
-                    ))
-                  }
-                </div>
+          ))}
+          {loading && (
+            <div className="flex relative select-none">
+              <div className="slr relative gap-x-10 flex overflow-x-scroll scroll-smooth backdrop-blur-md shadow-lg py-2">
+                {[...Array(7)].map((item, index) => (
+                  // skeleton loader
+                  <div key={index} className="w-[500px] h-[300px] relative">
+                    <div className="w-[350px] h-full bg-gray-400 animate-pulse"></div>
+                  </div>
+                ))}
               </div>
-              )
-            }
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Home;
